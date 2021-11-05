4763c30f801c1cab10525e3ababda049334be07a
alex <sasha@litnevski.studio>
2021-11-06 01:00:33 +0200

    added correct API key

--- src/js/ApiServices.js
+++ src/js/ApiServices.js
@@ -1,4 +1,4 @@
-const API_KEY = "a907caf8c46067564d1786718be1cb84";
+const API_KEY = "c3dfb97c4e42ba6803ce4a8859444b9b";
 const BASE_URL = "https://developers.themoviedb.org/3/";
 
 export default class APIService {
