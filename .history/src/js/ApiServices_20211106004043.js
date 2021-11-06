bde944d96085a9654f1dbf36e1dd0ac3d41950ed
alex <sasha@litnevski.studio>
2021-11-06 15:42:45 +0200

    added missed files

--- .history/src/js/ApiServices_20211106004043.js
+++
@@ -1,6 +0,0 @@
-export class APIService {
-    constructor() {
-      this.keyAPI = 'a907caf8c46067564d1786718be1cb84';
-      this.baseUrl = 'https://developers.themoviedb.org/3/';
-    }
-  }
--- .history/src/js/ApiServices_20211106004124.js
+++
@@ -1,9 +0,0 @@
-const API_KEY = "a907caf8c46067564d1786718be1cb84";
-const BASE_KEY = "a907caf8c46067564d1786718be1cb84";
-
-export default class APIService {
-    constructor() {
-        this.keyAPI = "a907caf8c46067564d1786718be1cb84";
-        this.baseUrl = "https://developers.themoviedb.org/3/";
-    }
-}
