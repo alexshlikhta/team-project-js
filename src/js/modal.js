0905764280577666ff94a280dc1c458de665b119
Dima Tytenko <amgpetronass@gmail.com>
2021-11-10 17:00:18 +0200

    add

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,51 +1,22 @@
 import * as basicLightbox from 'basiclightbox';
-import axios from 'axios';
-// =================axios=================//
-axios.defaults.baseURL = 'https://api.themoviedb.org/3';
-// const BASE_URL = 'https://api.themoviedb.org/3/';
+import ApiServices from './ApiServices';
+const dataApiServices = new ApiServices();
 
-const API_KEY = 'afc2024e71269fd3f1e3cbd71f7c4df4';
-const popularFilms = 'trending/movie/week';
-const endPoint = 'search/movie';
-const page = 1;
-const query = 'moto';
-
-// https: axios
-//   .get(`/${popularFilms}?api_key=${API_KEY}`)
-//   .then(res => console.log(res.data.results))
-//   .catch(err => console.log(err));
-
-// https: axios
-//   .get(`/${endPoint}?api_key=${API_KEY}&language=en-US&page=${page}&query=${query}`)
-//   .then(res => console.log(res.data.results))
-//   .catch(err => console.log(err));
-
-// =============================================
-
-const bodyRef = document.querySelector('body');
-
+const bodyRef = document.querySelector('.js-films');
 bodyRef.addEventListener('click', openLightbox);
 
-function openLightbox(event) {
+async function openLightbox(event) {
   event.preventDefault();
 
   if (event.target.nodeName !== 'IMG') {
     return;
   }
-  console.log(event.target.nodeName);
-  console.log(event.target.attributes.id.value);
+  dataApiServices.setMovieId(event.target.attributes.id.value);
+  const dataFilmById = await dataApiServices.fetchFilmById();
 
-  const movie_id = event.target.attributes.id.value;
-
-  axios
-    .get(`/movie/${movie_id}?api_key=${API_KEY}&language=en-US`)
-    .then(res => {
-      console.log(res.data.title);
-    })
-    .catch(err => console.log(err));
-
-  const instance = basicLightbox.create(`
-    <img src="${event.target.dataset.source}" width="800" height="600">
-`);
-  instance.show();
+  const markup = `
+      <img src="https://image.tmdb.org/t/p/w780${dataFilmById.poster_path}" width="800" height="600">
+    `;
+  const lightbox = basicLightbox.create(markup);
+  lightbox.show();
 }
