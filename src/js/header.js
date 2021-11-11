689a7bc54595f53f5e16841640244b46e2225668
Anastasiia Konstantynova <developer1302@gmail.com>
2021-11-11 14:02:28 +0200

    объединила хедер

--- src/js/header.js
+++ src/js/header.js
@@ -1,12 +1,39 @@
 const homeBtn = document.querySelector('.menu__btn');
 const myLibraryBtn = document.getElementById('my-library');
+const searchForm = document.querySelector('.search-form');
+const header = document.querySelector('.header');
+const libraryPage = document.querySelector('.library-page');
+const containerLib = document.querySelector('.container-lib');
+const watchedBtn = document.getElementById('watched');
+const queueBtn = document.getElementById('queue');
+
 
 homeBtn.addEventListener('click', () => {
   homeBtn.classList.add('current');
   myLibraryBtn.classList.remove('current');
+
+  searchForm.classList.remove('visually-hidden');
+  header.classList.remove('library-page');
+  containerLib.classList.add('visually-hidden');
 });
 
 myLibraryBtn.addEventListener('click', () => {
   myLibraryBtn.classList.add('current');
   homeBtn.classList.remove('current');
+
+  searchForm.classList.add('visually-hidden');
+  header.classList.add('library-page');
+  containerLib.classList.remove('visually-hidden');
 });
+
+watchedBtn.addEventListener('click', () => {
+  watchedBtn.classList.add('active');
+  queueBtn.classList.remove('active');
+});
+
+queueBtn.addEventListener('click', () => {
+  queueBtn.classList.add('active');
+  watchedBtn.classList.remove('active');
+});
+
+
