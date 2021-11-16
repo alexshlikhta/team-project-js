f39489b2059b95ce90ca6f3d4e12507a957da7dd
YaroslavLazorenko <lazorenko85@gmail.com>
2021-11-16 21:29:37 +0200

    Update RenderMarkup.js

--- src/js/RenderMarkup.js
+++ src/js/RenderMarkup.js
@@ -45,6 +45,11 @@ export default class RenderMarkup {
     loader.show();
     const dataPopular = await this.apiServices.fetchPopularFilms();
     refs.filmsList.innerHTML = cardTemplate(this.apiServices.transformData(dataPopular.results));
+    // Hide votes in popular films list
+    for (const elem of refs.votesSpan) {
+      elem.classList.add('is-hidden');
+    }
+
     loader.close();
   };
 }
